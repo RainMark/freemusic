7f469dc25731e9c74e908816cb6d94f55578e2d1
RainMark <rain@linuxstory.org>
2016-08-21 22:17:04 +0800

    Waiting for database class.

--- src/include/data_type.py
+++ src/include/data_type.py
@@ -6,11 +6,11 @@ type_dict = {   'login':                        '0001',
                 'register':                     '0004',
                 'register_success':             '0005',
                 'register_failed':              '0006',
-                'fetch_list':                   '0007',
-                'list_data':                    '0008',
+                'fetch_end':                    '0007',
+                'fetch_list':                   '0008',
                 'fetch_song':                   '0009',
-                'song_data':                    '0010',
-                'new_list':                     '0011',
-                'new_list_success':             '0012'
+                'new_list':                     '0010',
+                'new_list_success':             '0011',
+                'new_list_failed':              '0012'
 
                 }
--- src/server/python/server.py
+++ src/server/python/server.py
@@ -12,6 +12,7 @@ class music_srv:
         "Music Server class."
         port = 6666
         host = "127.0.0.1"
+        client_address = ['0.0.0.0', 6666]
         socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         message = bytearray(1024)
 
@@ -28,19 +29,41 @@ class music_srv:
                         nbytes, address = self.socket.recvfrom_into(self.message)
                         if not nbytes:
                                 continue
+                        self.client_address = address
                         data = self.message.decode('utf-8')
                         data = data.split('\0')[0]
                         data_list = data.split('\r\n')
+
                         if data_list[0] == type_dict['login']:
-                               result = self.login(data_list[1], data_list[2])
-                               self.response(result)
+                                result = self.login(data_list[1], data_list[2])
+                                self.response(result)
+                        elif data_list[0] == type_dict['register']:
+                                result = self.register(data_list[1], data_list[2], data_list[3])
+                                self.response(result)
+                        elif data_list[0] == type_dict['fetch_list']:
+                                result = self.send_user_list(data_list[1])
+                                self.response(result)
+                        elif data_list[0] == type_dict['new_list']:
+                                result = self.user_list_new(data_list[1], data_list[2])
+                                self.response(result)
+                        else:
+                                pass
                                 
         def response(self, result):
-                send_buffer = result + "\r\n"
-                self.socket.sendto()
+                send_buffer = result.encode('utf-8')
+                self.socket.sendto(send_buffer, self.client_address)
 
-        def login(self, user, password):
+        def login(self, email, password):
                 return "login_success"
+        
+        def register(self, email, password, nick):
+                return "register_success"
+
+        def send_user_list(self, email):
+                return "fetch_end"
+
+        def user_list_new(self, email, list_name):
+                return "new_list_success"
 
 srv = music_srv()
 srv.run()
