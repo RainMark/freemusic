7818f9c6ebfb929edd0cbc161f3cc82e248ffd6d
RainMark <rain@linuxstory.org>
2016-08-24 21:11:47 +0800

    Designed the UI again.

---
+++ src/client/python/ui.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+
+class hanlder:
+        def CloseApp(self, *args):
+                Gtk.main_quit(*args)
+
+class ui:
+        'Client UI class.'
+        builder = Gtk.Builder()
+
+        def __init__(self):
+                self.builder.add_from_file("../../../resources/free-music.ui")
+                self.builder.connect_signals(hanlder())
+                window = self.builder.get_object("window")
+                window.show_all()
+        
+        def main(self):
+                Gtk.main()
+
+ui = ui()
+ui.main()
